0b876f5a45887d3f96171778aeee02090d680a95
root <root@ip-172-31-34-150.ap-northeast-1.compute.internal>
2024-12-15 16:59:29 +0000

    project-added

--- angular-frontend/src/app/services/worker.service.ts
+++ angular-frontend/src/app/services/worker.service.ts
@@ -9,7 +9,7 @@ import { Worker } from '../models/worker';
 })
 export class WorkerService {
 
-  private getUrl: string = "http://localhost:8080/api/v1/workers";
+  private getUrl: string = "http://57.180.55.30:8080/api/v1/workers";
 
   constructor(private _httpClient: HttpClient) { }
 
